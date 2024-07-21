f7c2b385580d74b66d2ab30859732011326f0067
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-21 16:47:45 +0700

    modified dataset_preprocess.py -> add select_columns and save_path

--- dataset_preprocess.py
+++ dataset_preprocess.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 # Filter columns 
-def filtering_dataset(csv_path):
+def filtering_dataset(csv_path, selected_columns):
     df = pd.read_csv(csv_path)
-    filtered_df = df[["Hours Studied", "Sample Question Papers Practiced", "Previous Scores"]]
+    filtered_df = df[selected_columns]
     filtered_df.columns = ["x1", "x2", "y"]
     # print(filtered_df.head(5))
     return filtered_df
 
 # Split dataset
-def spliting_data(filtered_df):
+def spliting_data(filtered_df, save_path):
     # print(filtered_df.head(5))
     shuffled_df = filtered_df.sample(frac=1)
     # print(shuffled_df.head(5))
@@ -22,11 +22,14 @@ def spliting_data(filtered_df):
     # print(train.shape[0])
     test_df = shuffled_df[train_size:]
     # print(test.shape[0])
-    train_df.to_csv("train_dataset.csv", index=False) 
-    test_df.to_csv("test_dataset.csv", index=False) 
+    train_df.to_csv(f"{save_path}/train_dataset.csv", index=False) 
+    test_df.to_csv(f"{save_path}/test_dataset.csv", index=False) 
 
 
 if __name__ == "__main__":
     csv_path = "dataset.csv"
-    filtered_df = filtering_dataset(csv_path)
-    spliting_data(filtered_df)
+    # selected_columns = ["Hours Studied", "Sample Question Papers Practiced", "Previous Scores"]
+    selected_columns = ["number_courses", "time_study", "Marks"]
+    filtered_df = filtering_dataset(csv_path, selected_columns)
+    save_path = "datset2"
+    spliting_data(filtered_df, save_path)
