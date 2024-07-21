f2d9958f64d600e0d3e11a8bf9c33fe1ac381cec
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-21 13:05:31 +0700

    modify dataset_preprocess.py -> add filter dataset function

--- dataset_preprocess.py
+++ dataset_preprocess.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
+# Filter columns 
+def filtering_dataset(csv_path):
+    df = pd.read_csv(csv_path)
+    filtered_df = df[["Hours Studied", "Sample Question Papers Practiced", "Previous Scores"]]
+    filtered_df.columns = ["x1", "x2", "y"]
+    print(filtered_df.head(5))
+
+def spliting_data(filtered_df):
+    pass
+
 if __name__ == "__main__":
     csv_path = "dataset.csv"
+    filtered_df = filtering_dataset(csv_path)
