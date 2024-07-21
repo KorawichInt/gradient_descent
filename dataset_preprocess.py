533dfe08f9481d8a84acef2c5a520961ab6ff17a
KorawichInt <korawich.inthamusik@gmail.com>
2024-07-21 13:27:23 +0700

    modyfied dataset_preprocess.py -> add split_data function

--- dataset_preprocess.py
+++ dataset_preprocess.py
@@ -5,11 +5,28 @@ def filtering_dataset(csv_path):
     df = pd.read_csv(csv_path)
     filtered_df = df[["Hours Studied", "Sample Question Papers Practiced", "Previous Scores"]]
     filtered_df.columns = ["x1", "x2", "y"]
-    print(filtered_df.head(5))
+    # print(filtered_df.head(5))
+    return filtered_df
 
+# Split dataset
 def spliting_data(filtered_df):
-    pass
+    # print(filtered_df.head(5))
+    shuffled_df = filtered_df.sample(frac=1)
+    # print(shuffled_df.head(5))
+
+    total_rows = shuffled_df.shape[0]
+    train_size = int(total_rows*0.8)
+ 
+    # Split data into test and train
+    train_df = shuffled_df[0:train_size]
+    # print(train.shape[0])
+    test_df = shuffled_df[train_size:]
+    # print(test.shape[0])
+    train_df.to_csv("train_dataset", index=False) 
+    test_df.to_csv("test_dataset", index=False) 
+
 
 if __name__ == "__main__":
     csv_path = "dataset.csv"
     filtered_df = filtering_dataset(csv_path)
+    spliting_data(filtered_df)
